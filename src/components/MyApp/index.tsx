c9143befc578dc40af70c477e6ca6ce6357eac59
YuyaMurakami <villageup0623@gmail.com>
2025-07-28 02:43:35 +0900

    いったんreactにする

---
+++ src/components/MyApp/index.tsx
@@ -0,0 +1,10 @@
+export const MyApp = () => {
+  return (
+    <div>
+      <h1 className="title">いいとも！</h1>
+      <a href="/" className="linkBack">
+        トップへ戻る
+      </a>
+    </div>
+  );
+};
